3c107bd4528e99bfab3ffb44a18b517843facb0c
amandagitaa <amandagitasy@gmail.com>
2026-07-21 08:02:55 +0700

    fix share errors middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+
+        $middleware->web(append: [
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        ]);
+
         $middleware->alias([
             'admin' => \App\Http\Middleware\IsAdminMiddleware::class,
         ]);
